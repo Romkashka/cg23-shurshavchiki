d6323a971ff81a552e515d4d642344189b731b65
Romkashka <halilov.g.2020@gmail.com>
2023-09-27 00:51:31 +0300

    feat: add normalization to opened files

--- Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/util/P6DataReader.java
+++ Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/util/P6DataReader.java
@@ -1,6 +1,7 @@
 package ru.shurshavchiki.businessLogic.util;
 
 import ru.shurshavchiki.businessLogic.models.Header;
+import ru.shurshavchiki.businessLogic.models.MonochromePixel;
 import ru.shurshavchiki.businessLogic.models.RgbConvertable;
 import ru.shurshavchiki.businessLogic.models.RgbPixel;
 
@@ -8,6 +9,7 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class P6DataReader implements PixelDataReader {
 
@@ -19,7 +21,7 @@ public class P6DataReader implements PixelDataReader {
 
     public P6DataReader(Header header, java.io.File file) throws IOException {
         this.header = header;
-        this.totalPixels = header.getHeight() * header.getWidth();
+        this.totalPixels = (long) header.getHeight() * header.getWidth();
         FileInputStream fileInputStream = new FileInputStream(file);
         this.dataInputStream = new DataInputStream(fileInputStream);
 
@@ -50,7 +52,22 @@ public class P6DataReader implements PixelDataReader {
                 rgbPixelsList.add(new RgbPixel((byteData[6*i] & 0xff) * 256 + byteData[6*i+1]  & 0xff, (byteData[6*i+2] & 0xff) * 256 + byteData[6*i+3] & 0xff, (byteData[6*i+4] & 0xff) * 256 + byteData[6*i+5] & 0xff));
             }
         }
-        return rgbPixelsList;
+        return normalize(rgbPixelsList);
+    }
+
+    private ArrayList<RgbConvertable> normalize(List<RgbConvertable> rawData) {
+        ArrayList<RgbConvertable> result = new ArrayList<>(rawData.size());
+        for (RgbConvertable pixel: rawData) {
+            result.add(new RgbPixel(normalizeChannel(pixel.Red()),
+                    normalizeChannel(pixel.Green()),
+                    normalizeChannel(pixel.Blue())));
+        }
+
+        return result;
+    }
+
+    private int normalizeChannel(int value) {
+        return (int) (value * (255.0 / header.getMaxValue()));
     }
 
     @Override
