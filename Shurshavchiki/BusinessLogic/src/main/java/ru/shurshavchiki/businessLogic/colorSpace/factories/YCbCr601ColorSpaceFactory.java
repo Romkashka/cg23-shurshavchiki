1284b668ae81bf5c83b16b28cba6705722567b7f
Romkashka <halilov.g.2020@gmail.com>
2023-09-29 00:59:15 +0300

    feat: implement HSL, YcbCr601 and YcbCr709 converters

--- Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/colorSpace/factories/YCbCr601ColorSpaceFactory.java
+++ Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/colorSpace/factories/YCbCr601ColorSpaceFactory.java
@@ -2,6 +2,7 @@ package ru.shurshavchiki.businessLogic.colorSpace.factories;
 
 import ru.shurshavchiki.businessLogic.colorSpace.channelChoosers.ChannelChooserBuilder;
 import ru.shurshavchiki.businessLogic.colorSpace.converters.ColorSpaceConverter;
+import ru.shurshavchiki.businessLogic.colorSpace.converters.PredicateBasedConverter;
 import ru.shurshavchiki.businessLogic.colorSpace.models.Channel;
 import ru.shurshavchiki.businessLogic.colorSpace.models.ColorSpace;
 
@@ -21,11 +22,24 @@ public class YCbCr601ColorSpaceFactory implements ColorSpaceFactory {
 
     @Override
     public ChannelChooserBuilder getChannelChooserBuilder() {
-        return null;
+        return new ChannelChooserBuilder(getColorSpace(), List.of(0.0F, 128.0F, 128.0F));
     }
 
     @Override
     public ColorSpaceConverter getColorSpaceConverter() {
-        return null;
+        return new PredicateBasedConverter(
+                input -> {
+                    float Y =  0.299F * input[0] + 0.587F * input[1] + 0.114F * input[2];
+                    float Cb = (input[2] - Y) / 1.772F + 127.5F;
+                    float Cr = (input[0] - Y) / 1.402F + 127.5F;
+                    return new Float[]{Y, Cb, Cr};
+                },
+                input -> {
+                    float R = input[0] + 1.402F * (input[2] - 127.5F);
+                    float G = input[0] - 0.299F * 1.402F / 0.587F * (input[2] - 127.5F) - 0.114F * 1.772F / 0.587F * (input[1] - 127.5F);
+                    float B = input[0] + 1.772F * (input[1] - 127.5F);
+                    return new Float[]{R, G, B};
+                }
+        );
     }
 }
--- Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/colorSpace/factories/YCbCr709ColorSpaceFactory.java
+++ Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/colorSpace/factories/YCbCr709ColorSpaceFactory.java
@@ -2,6 +2,7 @@ package ru.shurshavchiki.businessLogic.colorSpace.factories;
 
 import ru.shurshavchiki.businessLogic.colorSpace.channelChoosers.ChannelChooserBuilder;
 import ru.shurshavchiki.businessLogic.colorSpace.converters.ColorSpaceConverter;
+import ru.shurshavchiki.businessLogic.colorSpace.converters.PredicateBasedConverter;
 import ru.shurshavchiki.businessLogic.colorSpace.models.Channel;
 import ru.shurshavchiki.businessLogic.colorSpace.models.ColorSpace;
 
@@ -21,11 +22,24 @@ public class YCbCr709ColorSpaceFactory implements ColorSpaceFactory {
 
     @Override
     public ChannelChooserBuilder getChannelChooserBuilder() {
-        return null;
+        return new ChannelChooserBuilder(getColorSpace(), List.of(0F, 127.5F, 127.5F));
     }
 
     @Override
     public ColorSpaceConverter getColorSpaceConverter() {
-        return null;
+        return new PredicateBasedConverter(
+                input -> {
+                    float Y =  0.2126F * input[0] + 0.7152F * input[1] + 0.0722F * input[2];
+                    float Cb = (input[2] - Y) / 1.8556F + 127.5F;
+                    float Cr = (input[0] - Y) / 1.5748F + 127.5F;
+                    return new Float[]{Y, Cb, Cr};
+                },
+                input -> {
+                    float R = input[0] + 1.5748F * (input[2] - 127.5F);
+                    float G = input[0] - 0.2126F * 1.5748F / 0.7152F * (input[2] - 127.5F) - 0.0722F * 1.8556F / 0.7152F * (input[1] - 127.5F);
+                    float B = input[0] + 1.8556F * (input[1] - 127.5F);
+                    return new Float[]{R, G, B};
+                }
+        );
     }
 }
