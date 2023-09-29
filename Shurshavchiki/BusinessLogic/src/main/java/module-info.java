ff2d7c9fa463b7a3a7a6a6b4bd81517e4d410840
GarnetAki <RomeNightingale@gmail.com>
2023-09-29 13:52:38 +0300

    Fix modules

--- Shurshavchiki/BusinessLogic/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module Shurshavchiki.BusinessLogic {
-    requires static lombok;
-    exports ru.shurshavchiki.businessLogic;
-    exports ru.shurshavchiki.businessLogic.services;
-    exports ru.shurshavchiki.businessLogic.entities;
-    exports ru.shurshavchiki.businessLogic.models;
-}
