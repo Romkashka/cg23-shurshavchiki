7c4cdac5fa16eb1f578f2994c52a4a5aee23957c
Romkashka <halilov.g.2020@gmail.com>
2023-11-08 16:22:16 +0300

    fix: add missing files

---
+++ Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/drawing/util/DrawingMerger.java
@@ -0,0 +1,11 @@
+package ru.shurshavchiki.businessLogic.drawing.util;
+
+import ru.shurshavchiki.businessLogic.domain.entities.Displayable;
+import ru.shurshavchiki.businessLogic.drawing.models.Drawing;
+import ru.shurshavchiki.businessLogic.drawing.models.FigureOverlap;
+
+public class DrawingMerger {
+    public Displayable addDrawing(Displayable source, Drawing drawing) {
+        throw new UnsupportedOperationException("Somebody will do it eventually (DrawingMerger)");
+    }
+}
