b9735c1809120370beb1d24b6bef3d35c7437a3d
Frost-Jack <71223785+Frost-Jack@users.noreply.github.com>
2023-12-21 21:51:22 +0300

    Revert "Added threshold filter"
    This reverts commit abdaa667acaa46663d6f37a78b187b80877eae3f.

--- Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/imageProcessing/filters/implementations/ThresholdFilter.java
+++ Shurshavchiki/BusinessLogic/src/main/java/ru/shurshavchiki/businessLogic/imageProcessing/filters/implementations/ThresholdFilter.java
@@ -45,24 +45,6 @@ public class ThresholdFilter extends ImageLinearFilterBase {
         );
     }
 
-    @Override 
-    public Displayable applyFilter(Displayable source) {
-        Displayable newDisplayable = source.clone();
-        List<List<RgbConvertable>> pixels = newDisplayable.getAllPixels();
-        for (int y = 0; y < source.getHeight(); y++){
-            for (int x = 0; x < source.getWidth(); x++){
-                RgbConvertable pixel = pixels.get(y).get(x);
-                RgbPixel newPixel = new RgbPixel(
-                    applyMaskValue(threshold1, threshold2, pixel.FloatRed),
-                    applyMaskValue(threshold1, threshold2, pixel.FloatGreen),
-                    applyMaskValue(threshold1, threshold2, pixel.FloatBlue)
-                );
-                newDisplayable.setPixel(x, y, newPixel);
-            }
-        }
-        return newDisplayable;
-    }
-
     @Override
     protected float applyMaskValue(int i, int j, float sourceValue) {
         if (sourceValue < threshold1) {
